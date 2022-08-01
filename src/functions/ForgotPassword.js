7c019d40bc65b8aad3480f2b8881935d6c1892f9
Christian <chrnielsen2003@gmail.com>
2022-08-02 01:42:19 +0200

    asd

--- src/functions/ForgotPassword.js
+++ src/functions/ForgotPassword.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import "./SignUp.scss";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ForgotPassword() {
   const emailRef = useRef();
